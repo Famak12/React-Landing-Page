5936c102ec56d27b5ebe16ef010feb6df2274c62
Fatima Manzoor <fatimamnzkayani@gmail.com>
2024-10-07 21:13:11 +0500

    header section complete

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,7 +11,16 @@ function Header() {
       <h3>CALL US TOLL FREE 0800 123-4567</h3>
     </div>
     <div className='head-container'>
-      
+      <div>
+        <h1>BOOK ONLINE</h1>
+        <p>Its safe, easy, and secure.</p>
+        <div>
+        <input type="text" />
+        <input type="submit" />
+        </div>
+        
+
+      </div>
 
     </div>
     
