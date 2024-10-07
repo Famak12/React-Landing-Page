b74de9484e4955706c736c680da54e724d61c09e
Fatima Manzoor <fatimamnzkayani@gmail.com>
2024-10-07 16:35:27 +0500

    style update

---
+++ src/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css'
+
+function Header() {
+  return (
+   <section>
+    <div className='head-row'>
+      <h3>
+        TRAVEL<span>PORTAL</span> 
+      </h3>
+      <h3>CALL US TOLL FREE 0800 123-4567</h3>
+    </div>
+    
+   </section>
+  )
+}
+
+export default Header;
