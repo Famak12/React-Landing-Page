b69bd07a9279e184701d5ce1d182601e7dc6a5cf
Fatima Manzoor <fatimamnzkayani@gmail.com>
2024-10-08 16:55:26 +0500

    middle section complete

--- src/MiddleContainer.jsx
+++ src/MiddleContainer.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Middlecontainer() {
+
     return (
         <section className="middle-container">
             <div> 
                 <h1 className="animate__animated animate__fadeIn animate__slower">Our Most Popular Destinations</h1>
             </div>
             <div className="destinations-div">
-                <div><img src="/images/Los-Angeles-at-night.jpg" alt="" style={{width: "350px", height: "300px"}}/> <p style={{color: "red"}}>Los Angeles</p> </div>
-                <div><img src="/images/London.jpg" alt="" style={{width: "350px", height: "300px"}}/>  <p style={{color: "red"}}>London</p> </div>
-                <div><img src="/images/zurich.jpg" alt="" style={{width: "350px", height: "300px"}} /> <p style={{color: "red"}}>Zurich</p> </div>
+                <div><img src="/images/Los-Angeles-at-night.jpg" alt=""/> <p>Los Angeles</p> </div>
+                <div><img src="/images/London.jpg" alt=""/>  <p>London</p> </div>
+                <div><img src="/images/zurich.jpg" alt=""/> <p>Zurich</p> </div>
             </div>
-           
+            <div className="about-div">
+                <h2>A Little About Us</h2>
+                <p>It is long established fact that a reader will be distracted by the readable content of a page when looking at its layout. 
+                    The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content 
+                    here, content here', making it look like readable English. Many desktopn publishing packages and web pages. </p>
+                <p style={{color: "red"}}>READ MORE</p>
 
+            </div>
         </section>
     )
 }
